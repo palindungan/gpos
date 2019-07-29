7faae280079fe0cc99e725c7c61d4a8b5cbcb54f
zalramli <github.little@gmail.com>
2019-07-29 19:15:44 +0700

    update kontak kami

--- application/views/tampilan/v_kontak_kami.php
+++ application/views/tampilan/v_kontak_kami.php
@@ -1,3 +1,11 @@
+<section class="section-bg section-mt">
+    <div class="jumbotron ">
+        <div class="container">
+            <h1 class="display-2">Contact Us</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+    </div>
+</section>
 <section id="team" class="section-bg">
     <div class="container">
         <br><br>
