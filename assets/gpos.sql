88c76c8df7ed28a3be92a45bdaae291b26e20c16
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-07 19:55:36 +0700

    setting database

---
+++ assets/gpos.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Agu 2019 pada 10.43
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gpos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `daftar_emas`
+--
+
+CREATE TABLE `daftar_emas` (
+  `id_emas` char(15) NOT NULL,
+  `id_user` char(8) NOT NULL,
+  `id_kat` char(8) NOT NULL,
+  `barcode` varchar(15) NOT NULL,
+  `kadar` int(2) NOT NULL,
+  `berat` float NOT NULL,
+  `hrg_emas` int(10) NOT NULL,
+  `stok` int(3) NOT NULL,
+  `tgl_input` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kat` char(8) NOT NULL,
+  `id_user` char(8) NOT NULL,
+  `nm_kat` varchar(25) NOT NULL,
+  `tgl_input` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `level_user`
+--
+
+CREATE TABLE `level_user` (
+  `id_level` char(3) NOT NULL,
+  `nm_level` varchar(20) NOT NULL,
+  `jml_max` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `toko_emas`
+--
+
+CREATE TABLE `toko_emas` (
+  `id_toko` char(4) NOT NULL,
+  `nm_toko` varchar(50) NOT NULL,
+  `almt_toko` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` char(8) NOT NULL,
+  `id_toko` char(4) NOT NULL,
+  `id_level` char(3) NOT NULL,
+  `nm_user` varchar(50) NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `email_user` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` char(60) NOT NULL,
+  `tgl_regist` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tgl_last_log_in` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `daftar_emas`
+--
+ALTER TABLE `daftar_emas`
+  ADD PRIMARY KEY (`id_emas`),
+  ADD UNIQUE KEY `barcode` (`barcode`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kat`);
+
+--
+-- Indeks untuk tabel `level_user`
+--
+ALTER TABLE `level_user`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indeks untuk tabel `toko_emas`
+--
+ALTER TABLE `toko_emas`
+  ADD PRIMARY KEY (`id_toko`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
