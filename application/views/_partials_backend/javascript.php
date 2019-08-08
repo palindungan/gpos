025e8a44b1c381a3da13524318e2900c97b73025
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-09 06:37:48 +0700

    menghapus javascript tingkat page ,,
    menghapus javascript tingkat page ,, js tingkat page harus di taruh di bawah views v_page.php yg spesifik

--- application/views/_partials_backend/javascript.php
+++ application/views/_partials_backend/javascript.php
@@ -6,16 +6,3 @@
 
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url() ?>assets/template_backend/js/sb-admin-2.min.js"></script>
-
-<!-- Page level plugins -->
-<script src="<?= base_url() ?>assets/template_backend/vendor/chart.js/Chart.min.js"></script>
-
-<!-- Page level custom scripts -->
-<script src="<?= base_url() ?>assets/template_backend/js/demo/chart-area-demo.js"></script>
-<script src="<?= base_url() ?>assets/template_backend/js/demo/chart-pie-demo.js"></script>
-
-<script src="<?= base_url() ?>assets/template_backend/vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="<?= base_url() ?>assets/template_backend/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-<!-- Page level custom scripts -->
-<script src="<?= base_url() ?>assets/template_backend/js/demo/datatables-demo.js"></script>
