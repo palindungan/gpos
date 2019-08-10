1b33d08c0a5c3d717df986c7f562e20646c72b08
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-11 06:09:26 +0700

    download resource
    download resource

--- application/views/_partials_frontend_wizard/head.php
+++ application/views/_partials_frontend_wizard/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <title>Bootstrap Wizard powered by Vue2</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css'>
+<link rel="stylesheet" href="<?= base_url() ?>assets/template_frontend_wizard/download/normalize.min.css">
+<link rel='stylesheet' href='<?= base_url() ?>assets/template_frontend_wizard/download/bootstrap.min.css'>
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 <link rel="stylesheet" href="<?= base_url() ?>assets/template_frontend_wizard/dist/style.css">
--- application/views/_partials_frontend_wizard/javascript.php
+++ application/views/_partials_frontend_wizard/javascript.php
@@ -1,3 +1,3 @@
 <!-- partial -->
-<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.4.4/vue.js'></script>
+<script src='<?= base_url() ?>assets/template_frontend_wizard/download/vue.js'></script>
 <script src="<?= base_url() ?>assets/template_frontend_wizard/dist/script.js"></script>
