1746d89bc884cedb1d0accd010f5fc3dd5ebeaca
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-11 02:46:28 +0700

    form data input daftar account
    form data input daftar account

--- application/views/tampilan_frontend/v_daftar.php
+++ application/views/tampilan_frontend/v_daftar.php
@@ -19,47 +19,18 @@
                     </div>
                     <div class="form-content">
                         <div class="form-header">
-                            <h3>Registration</h3>
+                            <h3>Resgistrasi Account</h3>
                         </div>
-                        <p>Please fill with your details</p>
+                        <p>Daftarkan toko emas anda</p>
                         <div class="form-row">
-                            <div class="form-holder">
-                                <input type="text" placeholder="First Name" class="form-control">
-                            </div>
-                            <div class="form-holder">
-                                <input type="text" placeholder="Last Name" class="form-control">
+                            <div class="form-holder w-100">
+                                <input type="text" id="nm_toko" name="nm_toko" placeholder="Nama Toko" class="form-control">
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="form-holder">
-                                <input type="text" placeholder="Your Email" class="form-control">
+                            <div class="form-holder w-100">
+                                <input type="text" id="almt_toko" name="almt_toko" placeholder="Alamat Toko" class="form-control">
                             </div>
-                            <div class="form-holder">
-                                <input type="text" placeholder="Phone Number" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-holder">
-                                <input type="text" placeholder="Age" class="form-control">
-                            </div>
-                            <div class="form-holder" style="align-self: flex-end; transform: translateY(4px);">
-                                <div class="checkbox-tick">
-                                    <label class="male">
-                                        <input type="radio" name="gender" value="male" checked> Male<br>
-                                        <span class="checkmark"></span>
-                                    </label>
-                                    <label class="female">
-                                        <input type="radio" name="gender" value="female"> Female<br>
-                                        <span class="checkmark"></span>
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="checkbox-circle">
-                            <label>
-                                <input type="checkbox" checked> Nor again is there anyone who loves or pursues or desires to obtaini.
-                                <span class="checkmark"></span>
-                            </label>
                         </div>
                     </div>
                 </div>
@@ -74,36 +45,33 @@
                     </div>
                     <div class="form-content">
                         <div class="form-header">
-                            <h3>Registration</h3>
+                            <h3>Resgistrasi Account</h3>
                         </div>
-                        <p>Please fill with additional info</p>
-                        <div class="form-row">
-                            <div class="form-holder w-100">
-                                <input type="text" placeholder="Address" class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-holder">
-                                <input type="text" placeholder="City" class="form-control">
-                            </div>
-                            <div class="form-holder">
-                                <input type="text" placeholder="Zip Code" class="form-control">
-                            </div>
-                        </div>
+                        <p>Isi data pribadi admin toko</p>
 
                         <div class="form-row">
-                            <div class="select">
+                            <div class="form-holder">
+                                <input type="text" id="nm_user" name="nm_user" placeholder="Nama User" class="form-control">
+                            </div>
+                            <div class="select" id="jenkel" name="jenkel">
                                 <div class="form-holder">
-                                    <div class="select-control">Your country</div>
+                                    <div class="select-control">Jenis Kelamin</div>
                                     <i class="zmdi zmdi-caret-down"></i>
                                 </div>
                                 <ul class="dropdown">
-                                    <li rel="United States">United States</li>
-                                    <li rel="United Kingdom">United Kingdom</li>
-                                    <li rel="Viet Nam">Viet Nam</li>
+                                    <li rel="">-</li>
+                                    <li rel="pria">Pria</li>
+                                    <li rel="wanita">Wanita</li>
                                 </ul>
                             </div>
-                            <div class="form-holder"></div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-holder">
+                                <input type="text" id="no_hp" name="no_hp" placeholder="No Hp" class="form-control">
+                            </div>
+                            <div class="form-holder">
+                                <input type="email" id="email_user" name="email_user" placeholder="Email" class="form-control">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -118,19 +86,23 @@
                     </div>
                     <div class="form-content">
                         <div class="form-header">
-                            <h3>Registration</h3>
+                            <h3>Resgistrasi Account</h3>
                         </div>
-                        <p>Send an optional message</p>
+                        <p>Isi data pribadi admin toko</p>
                         <div class="form-row">
                             <div class="form-holder w-100">
-                                <textarea name="" id="" placeholder="Your messagere here!" class="form-control" style="height: 99px;"></textarea>
+                                <input type="text" id="username" name="username" placeholder="Username" class="form-control">
                             </div>
                         </div>
-                        <div class="checkbox-circle mt-24">
-                            <label>
-                                <input type="checkbox" checked> Please accept <a href="#">terms and conditions ?</a>
-                                <span class="checkmark"></span>
-                            </label>
+                        <div class="form-row">
+                            <div class="form-holder w-100">
+                                <input type="password" id="password" name="password" placeholder="Password" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-holder w-100">
+                                <input type="password" id="k_password" name="k_password" placeholder="Konfirmasi Password" class="form-control">
+                            </div>
                         </div>
                     </div>
                 </div>
