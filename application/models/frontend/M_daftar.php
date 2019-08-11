b472de4d3afc401b859d9784a27c41ea1fc7a7c6
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-12 06:09:28 +0700

    Create M_daftar.php
    nambah

---
+++ application/models/frontend/M_daftar.php
@@ -0,0 +1,85 @@
+<?php
+class M_jalur_pembayaran extends CI_Model
+{
+
+    // mengambil semua data pada tabel
+    function tampil_data_toko_emas()
+    {
+        return $this->db->get('toko_emas');
+    }
+    function tampil_data_user()
+    {
+        return $this->db->get('user');
+    }
+
+    // untuk input data ke dalam database
+    function input_data($table, $data)
+    {
+        $this->db->insert($table, $data);
+    }
+
+    // untuk hapus data 
+    function hapus_data($table, $where)
+    {
+        // idnya
+        $this->db->where($where);
+
+        // tabelnya
+        $this->db->delete($table);
+    }
+
+    // untuk mengambil nilai data yg di edit
+    function edit_data($table, $where)
+    {
+        return $this->db->get_where($table, $where);
+    }
+
+    // untuk update data
+    function update_data($where, $data, $table)
+    {
+        $this->db->where($where);
+        $this->db->update($table, $data);
+    }
+
+    // autogenerate kode / ID
+    function get_no()
+    {
+        $field = "id_jalur";
+        $tabel = "jalur_pembayaran";
+        $digit = "2";
+        $kode = "J";
+
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel");
+        $kd = "";
+        if ($q->num_rows() > 0) {
+            foreach ($q->result() as $k) {
+                $tmp = ((int) $k->kd_max) + 1;
+                $kd = $kode . sprintf('%0' . $digit . 's',  $tmp);
+            }
+        } else {
+            $kd = "J01";
+        }
+        return $kd;
+    }
+
+    // autogenerate kode / ID
+    function get_no2()
+    {
+        $field = "id_jalur";
+        $tabel = "jalur_pembayaran";
+        $digit = "2";
+        $kode = "J";
+
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel");
+        $kd = "";
+        if ($q->num_rows() > 0) {
+            foreach ($q->result() as $k) {
+                $tmp = ((int) $k->kd_max) + 1;
+                $kd = $kode . sprintf('%0' . $digit . 's',  $tmp);
+            }
+        } else {
+            $kd = "J01";
+        }
+        return $kd;
+    }
+}
